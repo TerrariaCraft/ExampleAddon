54c9fec99287432e71eac41cdc3db44677726e06
Zeith <me@zeith.org>
2024-02-13 03:31:26 +0200

    Shove torch into blocks tab.

--- src/main/java/com/example/examplemod/init/BlocksEM.java
+++ src/main/java/com/example/examplemod/init/BlocksEM.java
@@ -1,7 +1,9 @@
 package com.example.examplemod.init;
 
 import com.example.examplemod.common.content.blocks.*;
+import com.google.common.base.Suppliers;
 import com.zeitheron.hammercore.annotations.*;
+import lombok.var;
 import org.zeith.terraria.api.items.IRareItem;
 import org.zeith.terraria.api.npc.Economy;
 import org.zeith.terraria.common.content.blocks.base.BlockBaseTC;
@@ -13,17 +15,26 @@ public class BlocksEM
 {
 	@RegistryName("example_block")
 	public static final ExampleBlock EXAMPLE_BLOCK = new ExampleBlock();
-
+	
 	@RegistryName("bricks/example")
 	public static final ExampleBrick EXAMPLE_BRICK = new ExampleBrick();
-
+	
 	@RegistryName("torches/example")
-	public static final TorchEntry EXAMPLE_TORCH = new TorchEntry()
-			.withColor(0x55FFFF);
+	public static final TorchEntry EXAMPLE_TORCH = torchToTab(new TorchEntry()
+			.withColor(0x55FFFF)
+	);
 	
 	@RegistryName("bars/example")
 	public static final BlockBaseTC EXAMPLE_BAR = new BlockBar()
 			.withValue(Economy.SILVER_VALUE)
 			.withRarity(IRareItem.ItemRarity.BLUE)
 			.inTab(ItemTabsEM.ITEMS_TAB, ItemTabsEM.BLOCKS_TAB);
+	
+	static TorchEntry torchToTab(TorchEntry entry) // All this method does is adding torch to the blocks tab.
+	{
+		var tabs = entry.getOn().tabs;
+		tabs.clear();
+		tabs.add(ItemTabsEM.BLOCKS_TAB);
+		return entry;
+	}
 }
